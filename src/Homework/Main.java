d918ef45758d1817d74092afb780b25860eb2e03
irina.kustova <irenekitty@yandex.ru>
2022-12-12 22:41:11 +0300

    homework done

---
+++ src/Homework/Main.java
@@ -0,0 +1,14 @@
+package Homework;
+
+public class Main {
+    public static void main(String[] args) {
+        Human person1 = new Human("Максим",1988, "Минск", "бренд-менеджер");
+        Human person2 = new Human("Аня", 1993, "Москва", "методист образовательных программ");
+        Human person3 = new Human("Катя", 1992, "Калининград", "продакт-менеджер");
+        Human person4 = new Human("Артём", 1995, "Москва", "директор по развитию бизнеса");
+        person1.introduction();
+        person2.introduction();
+        person3.introduction();
+        person4.introduction();
+    }
+}
